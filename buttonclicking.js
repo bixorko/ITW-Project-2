04996294370b0d3cb795cb11f2ad543adbcddaae
bixorko <peter.vinarcik99@gmail.com>
2020-04-20 22:44:27 +0200

    project done without errors with edited img

--- buttonclicking.js
+++ buttonclicking.js
@@ -1,15 +1,21 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function showDropDown() {
+    document.getElementById("dropdowmenu").classList.toggle("show");
 }
 
 window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
-        var myDropdown = document.getElementById("myDropdown");
+        var myDropdown = document.getElementById("dropdowmenu");
         if (myDropdown.classList.contains('show')) {
             myDropdown.classList.remove('show');
         }
     }
 }
+
+/**
+NA ZAKLADE TOHO JS KODU FUNGUJU (AKO PODLA PREDLOHY) PREPINANIA MEDZI MOBILNYM A DESKTOP VZHLADOM...
+HTML + CSS AUTOMATICKY ROZLISUJE ZARIADENIE NA KTOROM JE STRANKA SPUSTENA, ALE UZIVATEL MA MOZNOST
+ZAPNUT DESKTOP VERZIU NA MOBILE A OPACNE -> MOBILNU VERZIU NA DESKTOPE
+ */
 
 function changeSizeToMobile() {
     var el = document.getElementById("resizeme");
